ff14e6ffda3cb3d457c1babe9cb5d9e47264eb3d
Leonardo Tapia <barinas.code@gmail.com>
2019-02-19 20:12:10 +0000

    Download modules repositories logic

---
+++ install.sh
@@ -0,0 +1,16 @@
+echo "Initializing installator . . ."
+
+if find ./temp -mindepth 1 | read; then
+echo "Cleaning temp folder"
+sudo rm ./temp/* -r
+
+else
+   echo "Downloading modules"
+fi
+
+	cd temp
+	while read i;
+		do
+	git clone $i;
+
+	done < ../modules.txt

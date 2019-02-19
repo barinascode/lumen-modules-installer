707d32879d8afbf65e128efb294949b6cec8bd52
Leonardo Tapia <barinas.code@gmail.com>
2019-02-19 22:01:15 +0000

    Code refactory

--- install.sh
+++ install.sh
@@ -1,16 +1,44 @@
+clear
 echo "Initializing installator . . ."
 
+
 if find ./temp -mindepth 1 | read; then
-echo "Cleaning temp folder"
-sudo rm ./temp/* -r
+	echo "Cleaning temp folder"
+	sudo rm ./temp/* -r
 
 else
 	echo "Downloading modules"
-	cd temp
-
-while read i;
-                do
-        git clone $i;
-
-        done < ../modules.txt
 fi
+
+
+
+## Downloading modules
+cd temp
+while read i;
+    do
+	git clone $i;
+
+done < ../modules.txt
+cd ..
+
+
+echo "Installing modules"
+
+## installing modules ##
+
+for dir in temp/*
+do
+	echo -e "\t\t Installing ${dir}"
+
+	echo -e "\t\t Adding migrations files"
+	cp ./${dir}/database/migrations/* -r test/database/migrations
+
+	echo -e "\t\t Adding models files"
+	cp ./${dir}/app/*.php test/app
+
+	echo -e "\t\t Adding controllers files"
+	cp ./${dir}/app/Http/Controllers/* -r test/app/Http/Controllers
+
+	echo -e "\t\t Adding routes to web.php file"
+
+done
